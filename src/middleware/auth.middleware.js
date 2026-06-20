7b3ff2b1726250ec2d6ffd88a1fa2fa4170dd289
sadish panta <sadishpanta404@gmail.com>
2026-06-20 23:01:37 +0545

    setting authentication

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,4 +1,20 @@
-import bcrypt from "bcrypt"
 import jsonwebToken from "jsonwebtoken"
 import {User} from "../models/user.model"
+import { asyncHandler } from "../util/asyncHandler"
+import {ApiError} from "../util/ApiError"
 
+
+export const verifyJwt=asyncHandler(async(req,res,next)=>{
+    const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Brerer ", "");
+
+    if (!token){
+        throw new ApiError(401, "Unauthorized request")
+    }
+const decoded= jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+const user= await User.findById(decodedToken?._id).select ("-password -refreshToken")
+
+if (!user){
+        throw new ApiError(401, " You dont have valid accessToken")
+    }
+})
