e50a9ae761b9f20cc746bec6aa405250358bacc2
sadish panta <sadishpanta404@gmail.com>
2026-06-21 22:17:23 +0545

    Logout router created and updated in controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -73,12 +73,31 @@ return res.status(200)
 })
 
 const logOutUser=asyncHandler(async(req,res)=>{
-
+await User.findByIdAndUpdate(
+    req.user._id,
+    {
+        $set:{
+            refreshToken:undefined
+        }
+    },
+        {
+            new:true
+        }
     
+)
+const options={
+    httpOnly=true,
+    secure:true
+}
+return res.status(200)
+.clearCookie("accessToken",options)
+.clearCookie9("refreshToken",options)
+.json(new ApiResponse (200, {}, "User Logged Out Successfully"))
 })
 
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logOutUser
 }
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express"
 import { loginUser, registerUser } from "../controller/user.controller"
-
+import { verifyJWT } from "../middleware/auth.middleware"
 const router=Router()
 
 router.route("/register").post(registerUser)
 
 router.route("/login").post(loginUser)
 
+
+router.route("/logout").post(verifyJWT,logoutUser)
