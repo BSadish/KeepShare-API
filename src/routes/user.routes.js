2bf62a12ae296a936a60e3db2b8fd0d8c7f89f68
sadish panta <sadishpanta404@gmail.com>
2026-06-26 21:28:32 +0545

    new route added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,6 @@ const router=Router()
 router.route("/register").post(registerUser)
 
 router.route("/login").post(loginUser)
-
-
+router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
